66046ad5fb1d41b27ec6fbbb850eeaeca9590d47
Josh B <4ctrl.alt.del@gmail.com>
2016-09-17 15:32:33 -0700

    Added to clcontext class.
    Made comments for entire class.

--- src/clcontext.cpp
+++ src/clcontext.cpp
@@ -23,6 +23,8 @@ bool CLContext::is_initd()
 
 void CLContext::init_cl(CLDevice& dev)
 {
+   /// Create new OpenCL context, after which initialize both an OpenCL program and commandqueue.
+   /// If any initialization fails, reset object as an empty null object.
    static const char* f = __PRETTY_FUNCTION__;
    cl_context_properties props[] = {
       CL_CONTEXT_PLATFORM, (cl_context_properties)dev.platform(), 0 };
