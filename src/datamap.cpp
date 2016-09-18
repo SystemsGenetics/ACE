8c82cfbfad95a0c259dcf8ea4bb571829ef004f8
Josh B <4ctrl.alt.del@gmail.com>
2016-09-18 14:45:21 -0700

    Added to datamap class.
    Changed comments to new standard along with adding new comments,
    removing useless using lines, and adding spaces to boolean expressions.

--- src/datamap.cpp
+++ src/datamap.cpp
@@ -8,12 +8,6 @@ bool DataMap::_lock {false};
 
 
 
-/// @brief Initialize object manager.
-///
-/// Initializes object manager instance and makes sure there isn't already an
-/// instance of this class in existence.
-///
-/// @exception InvalidUse An instance of this class already exists.
 DataMap::DataMap(Factory& factory):
    _factory(factory),
    _i {_map.end()}
@@ -25,7 +19,6 @@ DataMap::DataMap(Factory& factory):
 
 
 
-/// Releases boolean lock so new instance can be made.
 DataMap::~DataMap()
 {
    _lock = false;
@@ -33,17 +26,7 @@ DataMap::~DataMap()
 
 
 
-/// Opens a data object file, optionally selected opened object.
-///
-/// @param file Name of file to be opened as data object.
-/// @param type Object file's data type.
-/// @param select True if newly opened data object will be selected, else false.
-///
-/// @exception AlreadyExists Data object with same file name as one given is
-/// already loaded in the manager.
-/// @exception InvalidType Data type cannot be found within the KINCPlugins
-/// system.
-Data* DataMap::open(const string& file, const string& type, bool select)
+Data* DataMap::open(const std::string& file, const std::string& type, bool select)
 {
    static const char* f = __PRETTY_FUNCTION__;
    using uptr = std::unique_ptr<Data>;
@@ -56,7 +39,7 @@ Data* DataMap::open(const string& file, const string& type, bool select)
    nd->init();
    auto x = _map.emplace(file,std::move(nd));
    auto i = x.first;
-   if (select)
+   if ( select )
    {
       _i = i;
    }
@@ -65,18 +48,11 @@ Data* DataMap::open(const string& file, const string& type, bool select)
 
 
 
-/// @brief Close data object.
-///
-/// Close an open data object, if data object is currently selected it is
-/// cleared to none.
-///
-/// @param file File name of opened data object to close.
-/// @return True if the closed data object was selected and cleared, else false.
-bool DataMap::close(const string& file)
+bool DataMap::close(const std::string& file)
 {
    bool ret = false;
    auto i = get(file);
-   if (_i==i)
+   if ( _i == i )
    {
       ret = true;
       _i = _map.end();
@@ -87,24 +63,17 @@ bool DataMap::close(const string& file)
 
 
 
-/// Select a data object ot have focus.
-///
-/// @param file Name of loaded data object to select.
-void DataMap::select(const string& file)
+void DataMap::select(const std::string& file)
 {
    _i = get(file);
 }
 
 
 
-/// Clear any selected data object to none.
-///
-/// @return True if there was a selected data object and it was cleared, else
-/// false.
 bool DataMap::unselect()
 {
    bool ret = false;
-   if (_i!=_map.end())
+   if ( _i != _map.end() )
    {
       ret = true;
       _i = _map.end();
@@ -114,12 +83,6 @@ bool DataMap::unselect()
 
 
 
-/// Passes load command to selected data object.
-///
-/// @param ops User command line.
-/// @param tm Program's terminal interface.
-///
-/// @exception NoSelect No data object is selected.
 void DataMap::load(GetOpts& ops, Terminal& tm)
 {
    static const char* f = __PRETTY_FUNCTION__;
@@ -139,12 +102,6 @@ void DataMap::load(GetOpts& ops, Terminal& tm)
 
 
 
-/// Passes dump command to selected data object.
-///
-/// @param ops User command line.
-/// @param tm Program's terminal interface.
-///
-/// @exception NoSelect No data object is selected.
 void DataMap::dump(GetOpts& ops, Terminal& tm)
 {
    static const char* f = __PRETTY_FUNCTION__;
@@ -164,12 +121,6 @@ void DataMap::dump(GetOpts& ops, Terminal& tm)
 
 
 
-/// Passes query command to selected data object.
-///
-/// @param ops User command line.
-/// @param tm Program's terminal interface.
-///
-/// @exception NoSelect No data object is selected.
 void DataMap::query(GetOpts& ops, Terminal& tm)
 {
    static const char* f = __PRETTY_FUNCTION__;
@@ -189,14 +140,7 @@ void DataMap::query(GetOpts& ops, Terminal& tm)
 
 
 
-/// @brief Find specific data object.
-///
-/// Searches list of loaded data object and returns pointer to data object that
-/// matches the file name given, if any.
-///
-/// @param file File name of data object to find.
-/// @return Pointer to data object that is found, else nullptr if not found.
-Data* DataMap::find(const string& file)
+Data* DataMap::find(const std::string& file)
 {
    try
    {
@@ -210,10 +154,6 @@ Data* DataMap::find(const string& file)
 
 
 
-/// Returns pointer to data object that is currently selected, if any.
-///
-/// @return Pointer to data object that is selected, else if no object is
-/// selected nullptr is returned.
 Data* DataMap::current()
 {
    static const char* f = __PRETTY_FUNCTION__;
@@ -224,9 +164,6 @@ Data* DataMap::current()
 
 
 
-/// Get beginning of list iterator.
-///
-/// @return Iterator.
 DataMap::Iterator DataMap::begin()
 {
    return _map.begin();
@@ -234,9 +171,6 @@ DataMap::Iterator DataMap::begin()
 
 
 
-/// Get end of list iterator.
-///
-/// @return Iterator.
 DataMap::Iterator DataMap::end()
 {
    return _map.end();
@@ -244,10 +178,6 @@ DataMap::Iterator DataMap::end()
 
 
 
-/// Return iterator of currently selected data object, if any.
-///
-/// @return Iterator of currently selected object, end of list iterator if no
-/// object is selected.
 DataMap::Iterator DataMap::selected()
 {
    return _i;
@@ -262,10 +192,7 @@ DataMap::Iterator DataMap::selected()
 ///
 /// @param file File name of data object to find.
 /// @return Iterator to data object that is found.
-///
-/// @exception DoesNotExist Data object with given file name does not exist in
-/// list of opened data objects.
-inline DataMap::Map::iterator DataMap::get(const string& file)
+inline std::map<std::string,std::unique_ptr<Data>>::iterator DataMap::get(const std::string& file)
 {
    static const char* f = __PRETTY_FUNCTION__;
    auto i = _map.find(file);
@@ -276,27 +203,20 @@ inline DataMap::Map::iterator DataMap::get(const string& file)
 
 
 
-/// Get file name of iterator's data object.
-///
-/// @return File name of data object.
-DataMap::Iterator::string DataMap::Iterator::file()
+std::string DataMap::Iterator::file()
 {
    return _i->first;
 }
 
 
 
-/// Get iterator's data object type.
-///
-/// @return Data object type.
-DataMap::Iterator::string DataMap::Iterator::type()
+std::string DataMap::Iterator::type()
 {
    return _i->second->type();
 }
 
 
 
-/// Iterate to next data object in list of objects.
 void DataMap::Iterator::operator++()
 {
    ++_i;
@@ -304,9 +224,6 @@ void DataMap::Iterator::operator++()
 
 
 
-/// Compare between two data object iterators.
-///
-/// @return False if they do not point to the same data object, else true.
 bool DataMap::Iterator::operator!=(const Iterator& cmp)
 {
    return _i!=cmp._i;
@@ -314,9 +231,6 @@ bool DataMap::Iterator::operator!=(const Iterator& cmp)
 
 
 
-/// Compare between two data object iterators.
-///
-/// @return True if they point to the same data object, else false.
 bool DataMap::Iterator::operator==(const Iterator& cmp)
 {
    return _i==cmp._i;
@@ -324,10 +238,12 @@ bool DataMap::Iterator::operator==(const Iterator& cmp)
 
 
 
+/// @brief Create new datamap iterator.
+///
 /// Initializes data object iterator from internal container iterator.
 ///
 /// @param i Internal container iterator.
-DataMap::Iterator::Iterator(Map::iterator i):
+DataMap::Iterator::Iterator(std::map<std::string,std::unique_ptr<Data>>::iterator i):
    _i(i)
 {}
 
