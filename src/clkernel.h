ce6d271e2ead43f8c3e672869ef8fd46716ba87f
Josh B <4ctrl.alt.del@gmail.com>
2016-09-17 18:57:30 -0700

    Added to clkernel class.
    Made comments for entire class along with adding spaces to boolean
    expressions.

--- src/clkernel.h
+++ src/clkernel.h
@@ -8,6 +8,7 @@ namespace AccelCompEng
 
 
 
+/// Wrapper for an OpenCL kernel.
 class CLKernel
 {
 public:
@@ -19,17 +20,48 @@ public:
    constexpr static int _maxDims {16};
    friend class CLCommandQueue;
    friend class CLProgram;
+   /// Create empty or null object that has no OpenCL kernel.
+   CLKernel() = default;
    ~CLKernel();
    CLKernel(const CLKernel&);
    CLKernel& operator=(const CLKernel&);
    CLKernel(CLKernel&&);
    CLKernel& operator=(CLKernel&&);
-   template<class T> void set_arg(cl_uint,T);
-   template<class T> void set_arg(cl_uint,CLBuffer<T>*);
+   /// Set argument to the kernel.
+   ///
+   /// @tparam T Value type being passed to kernel argument.
+   /// @param index Argument index of the kernel.
+   /// @param arg Value to set the kernel to. If the type is size_t it sets the argument as a local
+   /// variable. If the type is CLBuffer<T>* it sets the argument as the buffer object given.
+   template<class T> void set_arg(cl_uint index, T arg);
+   /// Set argument to the kernel.
+   ///
+   /// @tparam T Value type being passed to kernel argument.
+   /// @param index Argument index of the kernel.
+   /// @param arg Value to set the kernel to. If the type is size_t it sets the argument as a local
+   /// variable. If the type is CLBuffer<T>* it sets the argument as the buffer object given.
+   template<class T> void set_arg(cl_uint index, CLBuffer<T>* arg);
+   /// Set all arguments of kernel, much like calling a regular function.
+   ///
+   /// @tparam Args List of arguments to be passed to the kernel.
    template<class... Args> void set_args(Args...);
-   void set_swarm_dims(cl_uint);
-   void set_swarm_size(int,cl_uint,cl_uint);
+   /// Set number of dimensions for the kernel.
+   ///
+   /// @param dims number of dimensions, must at least be 1.
+   void set_swarm_dims(cl_uint dims);
+   /// Set global and local size of kernel for a given dimension.
+   ///
+   /// @param dim The dimension, starting at 0.
+   /// @param gSize The new global work size.
+   /// @param lSize The new local work size.
+   void set_swarm_size(int dim, cl_uint gSize, cl_uint lSize);
+   /// Get the workgroup size of this kernel for the user selected device.
+   ///
+   /// @return Largest local work size this kernel can handle.
    size_t get_wg_size();
+   /// Get suggested local size multiple of this kernel for the user selected device.
+   ///
+   /// @return Local work size multiple.
    size_t get_wg_multiple();
 private:
    CLKernel(cl_kernel,cl_device_id);
@@ -84,6 +116,10 @@ template<class... Args> void CLKernel::set_args(Args... args)
 
 
 
+/// Set final kernel argument from template list.
+///
+/// @tparam I Current parameter index.
+/// @tparam T Value type of current argument being fetched.
 template<int I,class T> void CLKernel::set_args_int(T arg)
 {
    set_arg(I,arg);
@@ -91,8 +127,12 @@ template<int I,class T> void CLKernel::set_args_int(T arg)
 
 
 
-template<int I,class T, class... Args>
-   void CLKernel::set_args_int(T arg, Args... args)
+/// Set kernel argument with next item in argument list, then pass to next.
+///
+/// @tparam I Current parameter index.
+/// @tparam T Value type of current argument being fetched.
+/// @tparam Args List of remaining arguments to be passed along.
+template<int I,class T, class... Args> void CLKernel::set_args_int(T arg, Args... args)
 {
    set_arg(I,arg);
    set_args_int<I+1>(args...);
