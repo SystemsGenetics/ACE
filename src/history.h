505e262c91dedf755d872f89bc83ce4eef78b38e
Josh B <4ctrl.alt.del@gmail.com>
2016-09-19 08:44:25 -0700

    Added to history class.
    Made comments for entire class.

--- src/history.h
+++ src/history.h
@@ -9,28 +9,51 @@ namespace AccelCompEng
 
 
 
+/// @brief History node.
+///
+/// Object that contains the history of a data object. It is recursive and contains child histories
+/// of previous input files going all the way back to the beginning.
 class History : private NVMemory::Node
 {
 public:
    struct AlreadySet : public Exception { using Exception::Exception; };
    struct NullMemory : public Exception { using Exception::Exception; };
    class Iterator;
+   /// Create empty history object.
    History();
+   /// Create empty history object with attached nvmemory.
+   /// @param mem File memory object.
    History(const std::shared_ptr<NVMemory>& mem);
+   /// Create history object with given nvmemory, loading history from given address.
+   /// @param mem File memory object.
+   /// @param ptr File address of history data location.
    History(const std::shared_ptr<NVMemory>& mem, int64_t ptr);
    History(const History&) = default;
    History(History&&) = default;
    History& operator=(const History&) = default;
    History& operator=(History&&) = default;
    using Node::mem;
+   /// Load new history data at the given address.
    void load(int64_t ptr);
+   /// Initialize a new history, this object must be empty to do this.
+   /// @param fileName File name of data object.
+   /// @param object The object which created the data object.
+   /// @param command The user command which created the data object.
    void init(const std::string& fileName, const std::string& object, const std::string& command);
+   /// Makes a new child by making a new copy of the given history.
    void add_child(const History& child);
+   /// Write the new history data to file memory. This object must be initialized with new history
+   /// data to do this.
    int64_t write();
+   /// Get file name of data object.
    const std::string& file_name() const;
+   /// Get object that created data object.
    const std::string& object() const;
+   /// Get user command that created data object.
    const std::string& command() const;
+   /// Get linux time stamp when this history was created.
    uint64_t time_stamp() const;
+   /// Does this history have any children?
    bool has_children() const;
    Iterator begin() const;
    Iterator end() const;
@@ -68,6 +91,7 @@ public:
    Iterator& operator=(Iterator&&) = default;
    Iterator begin() const;
    Iterator end() const;
+   /// Get reference to history object of this iterator.
    const History& history() const;
    const History& operator*() const;
    const History* operator->() const;
