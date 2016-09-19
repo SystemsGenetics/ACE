254b0400cf85ff4502ee3b2ff98abb986b8bfb8b
Josh B <4ctrl.alt.del@gmail.com>
2016-09-18 19:44:07 -0700

    Added to getopts class.
    Made comments for entire class along with adding spaces to boolean
    expressions.

--- src/getopts.cpp
+++ src/getopts.cpp
@@ -5,16 +5,7 @@ namespace AccelCompEng
 
 
 
-/// @brief Decomposes user line.
-///
-/// Decomposes the raw user input line given into commands and options, adding
-/// them to this new object's lists.
-///
-/// @param raw User input line.
-///
-/// @exception InvalidSyntax An error occured in the syntax of the user line
-/// while attempting to decompose it.
-GetOpts::GetOpts(const string& raw):
+GetOpts::GetOpts(const std::string& raw):
    _orig(raw)
 {
    static const char* f = __PRETTY_FUNCTION__;
@@ -22,16 +13,16 @@ GetOpts::GetOpts(const string& raw):
    using regtok = std::sregex_token_iterator;
    regex pat {"[\\s\\t]+"};
    regtok end {};
-   for (regtok r {raw.begin(),raw.end(),pat,-1};r!=end;++r)
+   for (regtok r {raw.begin(),raw.end(),pat,-1}; r != end ;++r)
    {
-      const string& i {*r};
-      if (i.front()=='-')
+      const std::string& i {*r};
+      if ( i.front() == '-' )
       {
-         string val;
-         string key;
+         std::string val;
+         std::string key;
          auto x = i.find_first_not_of('-');
          auto y = i.find('=');
-         if (y!=string::npos)
+         if ( y != std::string::npos )
          {
             bool cond = y==i.rfind('=')&&y>x;
             assert<InvalidSyntax>(cond,f,__LINE__);
@@ -53,19 +44,13 @@ GetOpts::GetOpts(const string& raw):
 
 
 
-/// Get origional user input line for this object.
-///
-/// @return Origional user input.
-const GetOpts::string& GetOpts::orig() const
+const std::string& GetOpts::orig() const
 {
    return _orig;
 }
 
 
 
-/// Get total number of commands left for this object.
-///
-/// @return Number of commands.
 int GetOpts::com_size() const
 {
    return _comms.size();
@@ -73,9 +58,6 @@ int GetOpts::com_size() const
 
 
 
-/// Tests if the commands list for this object is empty.
-///
-/// @return True if commands list is empty, else false.
 bool GetOpts::com_empty() const
 {
    return _comms.empty();
@@ -83,25 +65,16 @@ bool GetOpts::com_empty() const
 
 
 
-/// @brief Interrogate front command.
-///
-/// Interrogate the command at front of list for this object, comparing it to
-/// initializer list of strings given seeing if it matches any of them.
-///
-/// @param commands List of values to match front of list command by.
-/// @return 0 if no match is found in list of command values, else increment
-/// into initializer list where match was found starting with 1 for beginning
-/// of list.
-int GetOpts::com_get(initlist commands)
+int GetOpts::com_get(std::initializer_list<std::string> commands)
 {
    int ret = 0;
    int count = 0;
-   if (!_comms.empty())
+   if ( !_comms.empty() )
    {
       for (auto i:commands)
       {
          count++;
-         if (i==_comms.front())
+         if ( i == _comms.front() )
          {
             ret = count;
             break;
@@ -113,20 +86,16 @@ int GetOpts::com_get(initlist commands)
 
 
 
-/// Returns reference to string of command in front of list for this object.
-///
-/// @return Front of list command.
-GetOpts::string& GetOpts::com_front()
+std::string& GetOpts::com_front()
 {
    return _comms.front();
 }
 
 
 
-/// Remove the command at front of list of commands for this object.
 void GetOpts::com_pop()
 {
-   if (!_comms.empty())
+   if ( !_comms.empty() )
    {
       _comms.pop_front();
    }
@@ -134,9 +103,6 @@ void GetOpts::com_pop()
 
 
 
-/// Get total number of options left for this object.
-///
-/// @return Number of options.
 int GetOpts::size() const
 {
    return _opts.size();
@@ -144,9 +110,6 @@ int GetOpts::size() const
 
 
 
-/// Tests if the options list for this object is empty.
-///
-/// @return True if options list is empty, else false.
 bool GetOpts::empty() const
 {
    return _opts.empty();
@@ -154,21 +117,15 @@ bool GetOpts::empty() const
 
 
 
-/// Interrogate list of options to see if the key of any option matches the
-/// value given, optionally removing all matches from list of options if found.
-///
-/// @param opt Key value to match against.
-/// @param del True to remove all matching options, else false.
-/// @return True if one or more matches found, else false.
-bool GetOpts::has_opt(const string& opt, bool del)
+bool GetOpts::has_opt(const std::string& opt, bool del)
 {
    bool ret {false};
-   for (auto i = _opts.begin();i!=_opts.end();)
+   for (auto i = _opts.begin(); i != _opts.end() ;)
    {
-      if (i->first==opt)
+      if ( i->first == opt )
       {
          ret = true;
-         if (del)
+         if ( del )
          {
             i = _opts.erase(i);
          }
@@ -187,9 +144,6 @@ bool GetOpts::has_opt(const string& opt, bool del)
 
 
 
-/// Gets iterator for options list that points at the beginning of list.
-///
-/// @return Start of list options iterator.
 GetOpts::Iterator GetOpts::begin()
 {
    return {_opts.begin()};
@@ -197,9 +151,6 @@ GetOpts::Iterator GetOpts::begin()
 
 
 
-/// Gets iterator for options list that points to one past end of list.
-///
-/// @return One past end of list iterator.
 GetOpts::Iterator GetOpts::end()
 {
    return {_opts.end()};
@@ -207,11 +158,6 @@ GetOpts::Iterator GetOpts::end()
 
 
 
-/// Removes option from list by iterator given.
-///
-/// @param i Iterator that points to option that will be removed.
-/// @return Iterator that points to next option in list or one past end of list
-/// if the removed option was at back of list.
 GetOpts::Iterator GetOpts::erase(Iterator i)
 {
    return {_opts.erase(i._i)};
@@ -219,29 +165,20 @@ GetOpts::Iterator GetOpts::erase(Iterator i)
 
 
 
-/// Get key value of this option.
-///
-/// @return Key value.
-const GetOpts::string& GetOpts::Iterator::key() const
+const std::string& GetOpts::Iterator::key() const
 {
    return _i->first;
 }
 
 
 
-/// Tests if the given key value matches the key value of this option.
-///
-/// @return True if they match, else fail.
-bool GetOpts::Iterator::is_key(const string& cmp) const
+bool GetOpts::Iterator::is_key(const std::string& cmp) const
 {
    return _i->first==cmp;
 }
 
 
 
-/// Tests if the value for this option is empty or not.
-///
-/// @return True if there is no value set, else false.
 bool GetOpts::Iterator::val_empty() const
 {
    return _i->second.empty();
@@ -249,7 +186,6 @@ bool GetOpts::Iterator::val_empty() const
 
 
 
-/// Iterates to next option in list of options.
 void GetOpts::Iterator::operator++()
 {
    ++_i;
@@ -257,9 +193,6 @@ void GetOpts::Iterator::operator++()
 
 
 
-/// Tests if this option iterator is not equal to the given option iterator.
-///
-/// @return True if they are not equal, else fail.
 bool GetOpts::Iterator::operator!=(const Iterator& cmp)
 {
    return _i!=cmp._i;
@@ -267,9 +200,6 @@ bool GetOpts::Iterator::operator!=(const Iterator& cmp)
 
 
 
-/// Tests if this option iterator is equal to the given option iterator.
-///
-/// @return True if they are equal, else fail.
 bool GetOpts::Iterator::operator==(const Iterator& cmp)
 {
    return _i==cmp._i;
@@ -277,11 +207,7 @@ bool GetOpts::Iterator::operator==(const Iterator& cmp)
 
 
 
-/// Sets internal list iterator to iterator given.
-///
-/// @param i Internal iterator used to store pointer to option in list of
-/// iterators.
-GetOpts::Iterator::Iterator(const oplist::iterator& i):
+GetOpts::Iterator::Iterator(const std::list<std::pair<std::string,std::string>>::iterator& i):
    _i(i)
 {}
 
--- src/getopts.h
+++ src/getopts.h
@@ -10,119 +10,104 @@ namespace AccelCompEng
 
 
 
-/// @ingroup dataplugin
 /// @brief Decomposes line of user input.
 ///
-/// Takes a single line from the user and seperates it into commands and
-/// options. A command is any word seperated by either spaces or tabs that does
-/// not begin with a '-' character. An option is any word that begins with a '-'
-/// character, having a key and optional value. They key and value of an option
-/// are seperated using the '=' character. For example, 'one two --option=val'
-/// has two commands(one and two) and one option(with a key of option and a
-/// value of val).
+/// Takes a single line from the user and seperates it into commands and options. A command is any
+/// word seperated by either spaces or tabs that does not begin with a '-' character. An option is
+/// any word that begins with a '-' character, having a key and optional value. They key and value
+/// of an option are seperated using the '=' character. For example, 'one two --option=val' has two
+/// commands(one and two) and one option(with a key of option and a value of val).
 class GetOpts
 {
 public:
-   // *
-   // * EXCEPTIONS
-   // *
    struct InvalidType : public Exception { using Exception::Exception; };
    struct InvalidSyntax : public Exception { using Exception::Exception; };
-   // *
-   // * DECLERATIONS
-   // *
    class Iterator;
-   using string = std::string;
-   using initlist = std::initializer_list<string>;
-   // *
-   // * BASIC METHODS
-   // *
-   GetOpts(const string&);
+   /// Decomposes the raw user input line given into commands and options, adding them to this new
+   /// object's lists.
+   /// @param raw User input line.
+   GetOpts(const std::string& raw);
    GetOpts(const GetOpts&) = default;
    GetOpts(GetOpts&&) = default;
    GetOpts& operator=(const GetOpts&) = default;
    GetOpts& operator=(GetOpts&&) = default;
-   // *
-   // * FUNCTIONS
-   // *
-   const string& orig() const;
+   /// Get origional user input line for this object.
+   /// @return Origional user input.
+   const std::string& orig() const;
+   /// Get total number of commands left for this object.
+   /// @return Number of commands.
    int com_size() const;
+   /// Tests if the commands list for this object is empty.
+   /// @return True if commands list is empty, else false.
    bool com_empty() const;
-   int com_get(initlist);
-   string& com_front();
+   /// Interrogate the command at front of list for this object, comparing it to initializer list of
+   /// strings given seeing if it matches any of them.
+   /// @param commands List of values to match front of list command by.
+   /// @return 0 if no match is found in list of command values, else increment into initializer
+   /// list where match was found starting with 1 for beginning of list.
+   int com_get(std::initializer_list<std::string>);
+   /// Returns reference to string of command in front of list for this object.
+   /// @return Front of list command.
+   std::string& com_front();
+   /// Remove the command at front of list of commands for this object.
    void com_pop();
+   /// Get total number of options left for this object.
+   /// @return Number of options.
    int size() const;
+   /// Tests if the options list for this object is empty.
+   /// @return True if options list is empty, else false.
    bool empty() const;
-   bool has_opt(const string&,bool = false);
+   /// Interrogate list of options to see if the key of any option matches the value given,
+   /// optionally removing all matches from list of options if found.
+   /// @param opt Key value to match against.
+   /// @param del True to remove all matching options, else false.
+   /// @return True if one or more matches found, else false.
+   bool has_opt(const std::string&,bool = false);
    Iterator begin();
    Iterator end();
+   /// Removes option from list by iterator given.
+   /// @param i Iterator that points to option that will be removed.
+   /// @return Iterator that points to next option in list or one past end of list if the removed
+   /// option was at back of list.
    Iterator erase(Iterator);
 private:
-   // *
-   // * DECLERATIONS
-   // *
-   using clist = std::list<string>;
-   using oplist = std::list<std::pair<string,string>>;
-   // *
-   // * VARIABLES
-   // *
-   /// Origional line of user input.
-   string _orig;
-   /// List of commands.
-   clist _comms;
-   /// List of options.
-   oplist _opts;
+   std::string _orig;
+   std::list<std::string> _comms;
+   std::list<std::pair<std::string,std::string>> _opts;
 };
 
 
 
-/// Iterates through list of all options decomposed from user input.
+/// @brief Getopts iterator.
 ///
-/// @author Josh Burns
-/// @date 24 March 2016
+/// Iterates through list of all options decomposed from user input.
 class GetOpts::Iterator
 {
 public:
-   // *
-   // * DECLERATIONS
-   // *
    friend class GetOpts;
-   using string = GetOpts::string;
-   using oplist = GetOpts::oplist;
-   // *
-   // * FUNCTIONS
-   // *
-   const string& key() const;
+   /// Get key value of this option.
+   /// @return Key value.
+   const std::string& key() const;
+   /// Get value of this option, if any.
+   /// @tparam T The variable type for option's value.
+   /// @return The option's value.
    template<class T> T value() const;
-   bool is_key(const string&) const;
+   /// Tests if the given key value matches the key value of this option.
+   /// @return True if they match, else fail.
+   bool is_key(const std::string&) const;
+   /// Tests if the value for this option is empty or not.
+   /// @return True if there is no value set, else false.
    bool val_empty() const;
-   // *
-   // * OPERATORS
-   // *
    void operator++();
    bool operator!=(const Iterator&);
    bool operator==(const Iterator&);
 private:
-   // *
-   // * BASIC METHODS
-   // *
-   Iterator(const oplist::iterator&);
-   // *
-   // * VARIABLES
-   // *
-   /// Internal iterator that points to an option in GetOpts list.
-   oplist::iterator _i;
+   Iterator(const std::list<std::pair<std::string,std::string>>::iterator&);
+   std::list<std::pair<std::string,std::string>>::iterator _i;
 };
 
 
 
-/// Get value of this option, if any.
-///
-/// @param T The variable type for option's value.
-/// @return The option's value.
-///
-/// @exception InvalidType The given type does not match the option's value or
-/// there is no value set for this option.
 template<class T> T GetOpts::Iterator::value() const
 {
    static const char* f = __PRETTY_FUNCTION__;
