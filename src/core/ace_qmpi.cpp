58ec1f80798b0c9da44855e49ba187edd046ad88
Josh B <4ctrl.alt.del@gmail.com>
2018-02-20 14:07:24 -0800

    Added comments to qmpi class.

--- src/core/ace_qmpi.cpp
+++ src/core/ace_qmpi.cpp
@@ -1,5 +1,5 @@
-#include <mpi.h>
 #include "ace_qmpi.h"
+#include <mpi.h>
 #include "exception.h"
 
 
@@ -8,6 +8,9 @@ using namespace Ace;
 
 
 
+/*!
+ * This is a pointer to the single instance of this class. 
+ */
 QMPI* QMPI::_instance {nullptr};
 
 
@@ -15,6 +18,19 @@ QMPI* QMPI::_instance {nullptr};
 
 
 
+/*!
+ * This returns a reference to the singleton instance of this class. 
+ *
+ * @return Reference to the singleton instance of this class. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. If the instance pointer is null then create new instance of the class and 
+ *    set pointer to its address. 
+ *
+ * 2. Return reference to instance pointed to by the instance pointer. 
+ */
 QMPI& QMPI::instance()
 {
    if ( !_instance )
@@ -29,6 +45,11 @@ QMPI& QMPI::instance()
 
 
 
+/*!
+ * Returns size of MPI run. 
+ *
+ * @return Size of MPI run. 
+ */
 int QMPI::size() const
 {
    return _size;
@@ -39,6 +60,11 @@ int QMPI::size() const
 
 
 
+/*!
+ * Returns rank of this process to identify it within the MPI run. 
+ *
+ * @return Rank of this process. 
+ */
 int QMPI::rank() const
 {
    return _rank;
@@ -49,6 +75,11 @@ int QMPI::rank() const
 
 
 
+/*!
+ * Tests if this process is the master process (rank 0) of the MPI run. 
+ *
+ * @return Returns true if this process is the master else returns false. 
+ */
 bool QMPI::isMaster() const
 {
    return _rank == 0;
@@ -59,17 +90,22 @@ bool QMPI::isMaster() const
 
 
 
-bool QMPI::isSlave() const
-{
-   return _rank != 0;
-}
-
-
-
-
-
-
-void QMPI::sendData(int toRank, const QByteArray data)
+/*!
+ * This slot is called to send data to another process within the MPI run. 
+ *
+ * @param toRank  
+ *
+ * @param data  
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. Call the MPI system to send new byte data to the specified rank. If the 
+ *    call was successful then control is returned else go to step 2. 
+ *
+ * 2. Create exception detailing failure and throw it. 
+ */
+void QMPI::sendData(int toRank, const QByteArray& data)
 {
    if ( MPI_Send(data.data(),data.size(),MPI_CHAR,toRank,0,MPI_COMM_WORLD) )
    {
@@ -85,8 +121,41 @@ void QMPI::sendData(int toRank, const QByteArray data)
 
 
 
-QMPI::QMPI(QObject* parent):
-   QObject(parent)
+/*!
+ * This is the timer event function re implemented from the QObject class that 
+ * is called whenever the timer event is fired. This function is used to poll 
+ * for new data from other MPI processes without blocking and firing signals for 
+ * any new data received. 
+ *
+ * @param event  
+ */
+void QMPI::timerEvent(QTimerEvent* event)
+{
+   //TODO; poll if there are in MPI recv events
+}
+
+
+
+
+
+
+/*!
+ * This is the private and only constructor for this class. It is private so no 
+ * one except this class itself can make an instance of it thereby enforcing its 
+ * singleton property. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. Initialize the MPI system and if it failed throw an exception detailing 
+ *    the error. 
+ *
+ * 2. Querying the MPI system for the size and rank. If the query failed throw 
+ *    an exception detailing the error. 
+ *
+ * 3. Initialize a QObject timer event used for polling of new data received. 
+ */
+QMPI::QMPI()
 {
    if ( MPI_Init(nullptr,nullptr) )
    {
@@ -109,15 +178,6 @@ QMPI::QMPI(QObject* parent):
       e.setDetails(tr("MPI_Comm_rank failed."));
       throw e;
    }
-   startTimer(50);
+   startTimer(_timerPeriod);
 }
 
-
-
-
-
-
-void QMPI::timerEvent(QTimerEvent* event)
-{
-   //TODO; poll if there are in MPI recv events
-}
--- src/core/ace_qmpi.h
+++ src/core/ace_qmpi.h
@@ -1,11 +1,19 @@
-#ifndef ACE_MPI_H
-#define ACE_MPI_H
+#ifndef ACE_QMPI_H
+#define ACE_QMPI_H
 #include <QObject>
 
 
 
 namespace Ace
 {
+   /*!
+    * This is a wrapper to the MPI interface. It provides support for basic MPI 
+    * functionality including initializing MPI, getting rank and size, and sending 
+    * and receiving data. This provides sending and receiving of data by using Qt 
+    * slots and signals to allow for event handling through Qt's event handling 
+    * system. This is a singleton class which only has a single instance you can 
+    * access through a special static function. 
+    */
    class QMPI : public QObject
    {
       Q_OBJECT
@@ -14,20 +22,42 @@ namespace Ace
       int size() const;
       int rank() const;
       bool isMaster() const;
-      bool isSlave() const;
    signals:
-      void dataReceived(QByteArray data, int fromRank);
+      /*!
+       * This is signaled whenever new data is received by another MPI process. 
+       *
+       * @param data Stores the raw data received from another process. 
+       *
+       * @param fromRank The rank of the process that sent this data. 
+       */
+      void dataReceived(const QByteArray& data, int fromRank);
    public slots:
-      void sendData(int toRank, const QByteArray data);
-   private:
+      void sendData(int toRank, const QByteArray& data);
+   protected:
       virtual void timerEvent(QTimerEvent* event) override final;
-      explicit QMPI(QObject* parent = nullptr);
+   private:
+      explicit QMPI();
+      /*!
+       * This is the total size of the MPI run representing the total number of 
+       * processes part of it. 
+       */
       int _size;
+      /*!
+       * This is the rank of this process that identifies it between all other 
+       * processes part of the MPI run. 
+       */
       int _rank;
+      /*!
+       * This is the period, in milliseconds, between each time this class checks for 
+       * new data received from other MPI processes. 
+       */
+      constexpr static int _timerPeriod {50};
+      /*!
+       * This is a pointer to the single instance of this class. 
+       */
       static QMPI* _instance;
    };
 }
 
-
-
 #endif
+
