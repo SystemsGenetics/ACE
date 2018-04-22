d492b1df783fa96cc8522fc0f8f272995990e005
Josh B <4ctrl.alt.del@gmail.com>
2018-04-21 21:54:48 -0700

    Added comments to opencl event.
    As a result fixed a constructor bug in the class.

--- src/core/opencl_event.cpp
+++ src/core/opencl_event.cpp
@@ -13,8 +13,10 @@ using namespace OpenCL;
 
 
 /*!
+ * Constructs a new event using the given OpenCL event ID. 
  *
- * @param id  
+ * @param id The OpenCL event ID that this new event contains. This must be a valid 
+ *           OpenCL event that has one reference count. 
  */
 Event::Event(cl_event id):
    _id(new cl_event)
@@ -28,19 +30,25 @@ Event::Event(cl_event id):
 
 
 /*!
+ * Constructs a new event that is a copy of the given event. 
  *
- * @param other  
+ * @param other The other event object that is copied to this new event. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. If the other event is null then do nothing and exit, else go to the next 
+ *    step. 
+ *
+ * 2. Allocate this event's ID pointer, copy the other event's ID to this one, and 
+ *    retain the event ID. 
  */
-Event::Event(const Event& other):
-   _id(new cl_event)
+Event::Event(const Event& other)
 {
-   *_id = *other._id;
-   cl_int code {clRetainEvent(*_id)};
-   if ( code != CL_SUCCESS )
+   if ( other._id )
    {
-      E_MAKE_EXCEPTION(e);
-      fillException(&e,code);
-      throw e;
+      *_id = *other._id;
+      retain();
    }
 }
 
@@ -50,8 +58,10 @@ Event::Event(const Event& other):
 
 
 /*!
+ * Constructs a new event taking the OpenCL event or null state of the given event. 
  *
- * @param other  
+ * @param other The other event object whose data is taken and set to this new 
+ *              event. 
  */
 Event::Event(Event&& other):
    _id(other._id)
@@ -65,6 +75,12 @@ Event::Event(Event&& other):
 
 
 /*!
+ * Releases the underlying OpenCL event, reducing its reference count by one. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. If this object has an allocated event ID then release the ID and delete it. 
  */
 Event::~Event()
 {
@@ -81,26 +97,42 @@ Event::~Event()
 
 
 /*!
+ * Makes a copy of the given event object, releasing any OpenCL event this event 
+ * may currently contain. 
  *
- * @param other  
+ * @param other The other event object that is copied to this event. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. If this object contains an event ID then release it. 
+ *
+ * 2. If the given object is null then clear this object's event ID if any and 
+ *    exit, else go to the next step. 
+ *
+ * 3. If this object does not have an allocated event ID then allocate one. 
+ *
+ * 4. Copy and retain the given event object's OpenCL event ID. 
  */
 void Event::operator=(const Event& other)
 {
    if ( _id )
    {
-      clReleaseEvent(*_id);
+      release();
+   }
+   if ( other._id )
+   {
+      if ( !_id )
+      {
+         _id = new cl_event;
+      }
+      *_id = *other._id;
+      retain();
    }
    else
    {
-      _id = new cl_event;
-   }
-   *_id = *other._id;
-   cl_int code {clRetainEvent(*_id)};
-   if ( code != CL_SUCCESS )
-   {
-      E_MAKE_EXCEPTION(e);
-      fillException(&e,code);
-      throw e;
+      delete _id;
+      _id = nullptr;
    }
 }
 
@@ -110,14 +142,23 @@ void Event::operator=(const Event& other)
 
 
 /*!
+ * Takes the OpenCL event or null state of the given event object releasing any 
+ * OpenCL event this event may currently contain. 
  *
- * @param other  
+ * @param other The other event object whose OpenCL event or null state is taken. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. If this object has an event ID then release and delete it. 
+ *
+ * 2. Take the given even object's OpenCL event ID. 
  */
 void Event::operator=(Event&& other)
 {
    if ( _id )
    {
-      clReleaseEvent(*_id);
+      release();
       delete _id;
    }
    _id = other._id;
@@ -130,6 +171,9 @@ void Event::operator=(Event&& other)
 
 
 /*!
+ * Tests if this event is null and does not contain an OpenCL event. 
+ *
+ * @return True if this event is null or false otherwise. 
  */
 bool Event::isNull() const
 {
@@ -142,6 +186,14 @@ bool Event::isNull() const
 
 
 /*!
+ * Waits for this object's OpenCL event to complete, blocking until it does. If 
+ * this event is null then this returns immediately. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. If this object is not null then wait for its event to complete. If waiting 
+ *    fails then throw an exception. 
  */
 void Event::wait() const
 {
@@ -163,6 +215,20 @@ void Event::wait() const
 
 
 /*!
+ * Tests if this object's OpenCL event is complete. 
+ *
+ * @return True if this event is complete or null, otherwise false. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. If this event is null then return true, else go to the next step. 
+ *
+ * 2. Get the status of this object's OpenCL event. If the OpenCL call fails then 
+ *    throw an exception. 
+ *
+ * 3. Return true if this object's OpenCL event status is complete or false 
+ *    otherwise. 
  */
 bool Event::isDone() const
 {
@@ -182,4 +248,52 @@ bool Event::isDone() const
       throw e;
    }
    return status == CL_COMPLETE;
+}
+
+
+
+
+
+
+/*!
+ * Reduces this object's OpenCL reference count by one. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. Release this object's OpenCL event, throwing an exception if it fails. 
+ */
+void Event::release()
+{
+   cl_int code {clRetainEvent(*_id)};
+   if ( code != CL_SUCCESS )
+   {
+      E_MAKE_EXCEPTION(e);
+      fillException(&e,code);
+      throw e;
+   }
+}
+
+
+
+
+
+
+/*!
+ * Increases this object's OpenCL reference count by one. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. Retain this object's OpenCL event, throwing an exception if it fails. 
+ */
+void Event::retain()
+{
+   cl_int code {clRetainEvent(*_id)};
+   if ( code != CL_SUCCESS )
+   {
+      E_MAKE_EXCEPTION(e);
+      fillException(&e,code);
+      throw e;
+   }
 }
--- src/core/opencl_event.h
+++ src/core/opencl_event.h
@@ -8,11 +8,18 @@
 namespace OpenCL
 {
    /*!
+    * This can contain an OpenCL event or be set to null. An event this class contains 
+    * can be copied or moved to another instance of this class. Copying an instance 
+    * with an OpenCL event simply means copying the ID and calling the retain method. 
+    * Once all instances referencing an OpenCL event are deleted the underlying event 
+    * is discarded. This class also provides utility methods for testing or waiting on 
+    * its event, along with testing to see if it is null. 
     */
    class Event
    {
    public:
       /*!
+       * Constructs a new event that is set to null (no OpenCL event). 
        */
       Event() = default;
       Event(cl_event id);
@@ -25,7 +32,11 @@ namespace OpenCL
       void wait() const;
       bool isDone() const;
    private:
+      void release();
+      void retain();
       /*!
+       * Pointer the OpenCL event ID of this object or null of this object is null and 
+       * contains no event. 
        */
       cl_event* _id {nullptr};
    };
