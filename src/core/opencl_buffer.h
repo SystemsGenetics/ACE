34950430a981be8164401672c78a993b3eac5132
Josh B <4ctrl.alt.del@gmail.com>
2018-04-22 13:15:17 -0700

    Added comments to opencl buffer.

--- src/core/opencl_buffer.h
+++ src/core/opencl_buffer.h
@@ -13,13 +13,22 @@
 namespace OpenCL
 {
    /*!
+    * This can contain an OpenCL buffer object or be set to null. The buffer this 
+    * class contains can be moved to another instance but not copied. This is a 
+    * template class to enforce strong type checking of the underlying buffer data. 
+    * This class provides methods for mapping the OpenCL buffer memory to a temporary 
+    * host pointer and can be accessed through other helper methods that provides 
+    * range checking or through a raw memory pointer. Other methods are provided for 
+    * getting the size of the buffer and the underlying OpenCL memory ID used by other 
+    * OpenCL classes. 
     *
-    * @tparam T  
+    * @tparam T The type used to create the buffer data of this object. 
     */
    template<class T> class Buffer
    {
    public:
       /*!
+       * Constructs a new buffer object that is set to null. 
        */
       Buffer() = default;
       Buffer(Context* context, int size);
@@ -40,18 +49,28 @@ namespace OpenCL
       void clear();
       void nullify();
       /*!
+       * Pointer to the OpenCL buffer ID of this object. If this object is null then this 
+       * is null. 
        */
       cl_mem* _id {nullptr};
       /*!
+       * Host memory pointer to this object's mapped OpenCL buffer. If this object is not 
+       * mapped then this is null. 
        */
       T* _data {nullptr};
       /*!
+       * Size of this object's OpenCL buffer as the number of elements of the defined 
+       * template type. 
        */
       int _size {-1};
       /*!
+       * OpenCL command queue ID of the last command queue to be used for mapping this 
+       * object's buffer. This is used to unmap the buffer if this object is destroyed. 
        */
       cl_command_queue _last;
       /*!
+       * The type of mapping of this object's OpenCL buffer if it is mapped. If this 
+       * object is not mapped this is not used and undefined. 
        */
       cl_map_flags _mapping;
    };
@@ -62,10 +81,19 @@ namespace OpenCL
 
 
    /*!
+    * Constructs a new buffer object set to a new OpenCL buffer with the given context 
+    * and size. 
     *
-    * @param context  
+    * @param context Pointer to context that this buffer object is created from. 
     *
-    * @param size  
+    * @param size The size of the buffer created as the number of elements of the 
+    *             template type defined. 
+    *
+    *
+    * Steps of Operation: 
+    *
+    * 1. Create a new OpenCL buffer and set this object's buffer ID to the new buffer. 
+    *    If creation fails then throw an exception. 
     */
    template<class T> Buffer<T>::Buffer(Context* context, int size):
       _id(new cl_mem),
@@ -87,8 +115,11 @@ namespace OpenCL
 
 
    /*!
+    * Constructs a new buffer object taking the state of the other given buffer 
+    * object. 
     *
-    * @param other  
+    * @param other The other buffer object whose state, null or set to an OpenCL 
+    *              buffer, is taken by this new buffer object. 
     */
    template<class T> Buffer<T>::Buffer(Buffer<T>&& other):
       _id(other._id),
@@ -106,6 +137,7 @@ namespace OpenCL
 
 
    /*!
+    * Clears all resources this buffer object may contain. 
     */
    template<class T> Buffer<T>::~Buffer()
    {
@@ -118,8 +150,17 @@ namespace OpenCL
 
 
    /*!
+    * Takes the state of the given buffer object , null or set to an OpenCL buffer, 
+    * overwriting and clearing any OpenCL buffer this object may already contain. 
     *
-    * @param other  
+    * @param other The other buffer object whose state, null or set to an OpenCL 
+    *              buffer, is taken by this buffer object. 
+    *
+    *
+    * Steps of Operation: 
+    *
+    * 1. Clear this object of any OpenCL resources, take the other object's state, and 
+    *    set the other object to null. 
     */
    template<class T> void Buffer<T>::operator=(Buffer<T>&& other)
    {
@@ -138,8 +179,20 @@ namespace OpenCL
 
 
    /*!
+    * Returns a reference to the element with the given index in this buffer object's 
+    * mapped data for writing. If this buffer object is not mapped, the mapping is not 
+    * for writing, or the index is out of range then an exception is thrown. 
     *
-    * @param index  
+    * @param index The index of the element whose reference is returned. 
+    *
+    * @return Reference to element of mapped buffer with the given index. 
+    *
+    *
+    * Steps of Operation: 
+    *
+    * 1. If this object is not mapped, the mapping is not for writing, or the given 
+    *    index is out of range then throw an exception, else return a reference to the 
+    *    mapped element with the given index. 
     */
    template<class T> T& Buffer<T>::operator[](int index)
    {
@@ -175,8 +228,21 @@ namespace OpenCL
 
 
    /*!
+    * Returns a read only reference to the element with the given index in this buffer 
+    * object's mapped data for reading. If this buffer object is not mapped, the 
+    * mapping is not for reading, or the index is out of range then an exception is 
+    * thrown. 
     *
     * @param index  
+    *
+    * @return Read only reference to element of mapped buffer with the given index. 
+    *
+    *
+    * Steps of Operation: 
+    *
+    * 1. If this object is not mapped, the mapping is not for reading, or the given 
+    *    index is out of range then throw an exception, else return a read only 
+    *    reference to the mapped element with the given index. 
     */
    template<class T> const T& Buffer<T>::at(int index) const
    {
@@ -212,6 +278,9 @@ namespace OpenCL
 
 
    /*!
+    * Tests if this buffer object is null or contains an OpenCL buffer. 
+    *
+    * @return True if this buffer object is null or false otherwise. 
     */
    template<class T> bool Buffer<T>::isNull() const
    {
@@ -224,6 +293,16 @@ namespace OpenCL
 
 
    /*!
+    * Returns the OpenCL buffer ID of this object. If this object is null then an 
+    * exception is thrown. 
+    *
+    * @return OpenCL buffer ID of this object. 
+    *
+    *
+    * Steps of Operation: 
+    *
+    * 1. If this object is null then throw an exception, else return this object's 
+    *    OpenCL buffer ID. 
     */
    template<class T> cl_mem Buffer<T>::id() const
    {
@@ -243,6 +322,11 @@ namespace OpenCL
 
 
    /*!
+    * Returns the size of this object's OpenCL buffer as the number of elements of the 
+    * template type defined. If this object is null then -1 is returned. 
+    *
+    * @return The number of elements this buffer object contains or -1 if this object 
+    *         is null. 
     */
    template<class T> int Buffer<T>::size() const
    {
@@ -255,8 +339,16 @@ namespace OpenCL
 
 
    /*!
+    * Maps this object's OpenCL buffer data to a host memory pointer for reading that 
+    * can be accessed through other methods of this class. The command to map memory 
+    * is sent to the given command queue and is not complete until the returned event 
+    * is complete. If this object is null or is already mapped then an exception is 
+    * thrown. 
     *
-    * @param queue  
+    * @param queue Pointer to the command queue used to add the OpenCL command for 
+    *              mapping the memory. 
+    *
+    * @return The event for the mapping command sent to the given command queue. 
     */
    template<class T> Event Buffer<T>::mapRead(CommandQueue* queue)
    {
@@ -269,8 +361,16 @@ namespace OpenCL
 
 
    /*!
+    * Maps this object's OpenCL buffer data to a host memory pointer for writing that 
+    * can be accessed through other methods of this class. The command to map memory 
+    * is sent to the given command queue and is not complete until the returned event 
+    * is complete. If this object is null or is already mapped then an exception is 
+    * thrown. 
     *
-    * @param queue  
+    * @param queue Pointer to the command queue used to add the OpenCL command for 
+    *              mapping the memory. 
+    *
+    * @return The event for the mapping command sent to the given command queue. 
     */
    template<class T> Event Buffer<T>::mapWrite(CommandQueue* queue)
    {
@@ -283,8 +383,28 @@ namespace OpenCL
 
 
    /*!
+    * Unmaps this object's OpenCL buffer data from host memory. The command to unmap 
+    * memory is sent to the given command queue and is not complete until the returned 
+    * event is complete. If the object is null or is not mapped then an exception is 
+    * thrown. 
     *
-    * @param queue  
+    * @param queue Pointer to the command queue used to add the OpenCL command for 
+    *              unmapping the memory. 
+    *
+    * @return The event for the unmapping command sent to the given command queue. 
+    *
+    *
+    * Steps of Operation: 
+    *
+    * 1. If this object is null or is not mapped then throw an exception, else go to 
+    *    the next step. 
+    *
+    * 2. Add an unmap command to the given command queue for this object's OpenCL 
+    *    buffer. If adding the command fails then throw an exception. 
+    *
+    * 3. Release the OpenCL command queue used to map this object's buffer and return 
+    *    the event of the unmapping command added to the given command queue in the 
+    *    last step. If releasing the command queue fails then throw an exception. 
     */
    template<class T> Event Buffer<T>::unmap(CommandQueue* queue)
    {
@@ -321,6 +441,18 @@ namespace OpenCL
 
 
    /*!
+    * Returns the host memory pointer of this object's mapped OpenCL buffer data. If 
+    * this object's buffer is not mapped then an exception is thrown. Care must be 
+    * taken to only read or write to this pointer depending on what type of mapping it 
+    * is. Reading from a write mapping or writing to a read mapping is undefined. 
+    *
+    * @return Pointer to host memory mapping of this object's OpenCL buffer. 
+    *
+    *
+    * Steps of Operation: 
+    *
+    * 1. If this object is not mapped then throw an exception, else return the host 
+    *    memory pointer of this object's mapped OpenCL buffer. 
     */
    template<class T> T* Buffer<T>::data()
    {
@@ -340,10 +472,28 @@ namespace OpenCL
 
 
    /*!
+    * Maps this object's OpenCL buffer to host memory using the given command queue to 
+    * add the command and the given mapping to set it for reading or writing. If this 
+    * object is null or is already mapped then an exception is thrown. 
     *
-    * @param queue  
+    * @param queue Pointer to the command queue used to add the mapping command. 
     *
-    * @param mapping  
+    * @param mapping The type of mapping used, either read or write. 
+    *
+    * @return The event for the mapping command added to the given command queue. 
+    *
+    *
+    * Steps of Operation: 
+    *
+    * 1. If this object is null or is already mapped then throw an exception, else go 
+    *    to the next step. 
+    *
+    * 2. Add a map command to the given command queue for this object's OpenCL buffer 
+    *    with the given mapping. If adding the command fails then throw an exception. 
+    *
+    * 3. Save the given command queue ID to this object, retain it, and return the 
+    *    event of the mapping command added to the given command queue in the last 
+    *    step. If retaining the command queue fails then throw an exception. 
     */
    template<class T> Event Buffer<T>::map(CommandQueue* queue, cl_map_flags mapping)
    {
@@ -397,6 +547,19 @@ namespace OpenCL
 
 
    /*!
+    * Clears any mapping of this object's OpenCL buffer if it has one, releases all 
+    * OpenCL resources, and deletes the OpenCL memory ID variable. This does not set 
+    * any of this object's pointers to null. 
+    *
+    *
+    * Steps of Operation: 
+    *
+    * 1. If this object is null then do nothing and exit, else go to the next step. 
+    *
+    * 2. If this object is mapped then call an unmap command with this object's saved 
+    *    command queue and then release it. 
+    *
+    * 3. Release this object's OpenCL buffer ID and then delete it. 
     */
    template<class T> void Buffer<T>::clear()
    {
@@ -418,6 +581,13 @@ namespace OpenCL
 
 
    /*!
+    * Sets this object to a null state by changing all its pointers to null. This does 
+    * not free any allocated memory those pointers may be pointing to. 
+    *
+    *
+    * Steps of Operation: 
+    *
+    * 1. Set all this object's pointers and size to the null state. 
     */
    template<class T> void Buffer<T>::nullify()
    {
