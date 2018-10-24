0d6a4969a3be9aef9dc2ccc8e924ed8180d5fcdd
Josh B <4ctrl.alt.del@gmail.com>
2018-10-23 19:07:38 -0700

    Added comments.
    Finished adding comments to socket class and main function in acelog
    application.

--- src/log/main.cpp
+++ src/log/main.cpp
@@ -7,15 +7,24 @@
 
 
 /*!
+ * Implements the main function. 
  *
- * @param argc  
+ * @param argc See GCC docs. 
  *
- * @param argv  
+ * @param argv See GCC docs. 
+ *
+ * @return See GCC docs. 
  */
 int main(int argc, char** argv)
 {
+   // Initialize qt core application. 
    QCoreApplication application(argc,argv);
+
+   // Create a new client and connect its destroyed signal to the application quit 
+   // slot. 
    Client* client {new Client};
    QObject::connect(client,&QObject::destroyed,&application,&QCoreApplication::quit);
+
+   // Execute the application. 
    return application.exec();
 }
--- src/log/socket.h
+++ src/log/socket.h
@@ -7,6 +7,10 @@
 
 
 /*!
+ * This is the client logging socket for the ACE logging framework. This is derived 
+ * from a qt TCP socket class with an additional signal when it receives a 
+ * formatted logging server message. All other functionality required, such as 
+ * connecting to a server, is in the base qt socket class. 
  */
 class Socket : public QTcpSocket
 {
@@ -15,12 +19,16 @@ public:
    Socket();
 signals:
    /*!
+    * Signals that the client has received a message from its connected logging 
+    * server. 
     *
-    * @param type  
+    * @param type The message type that was received. 
     *
-    * @param thread  
+    * @param thread The thread where the message originated on the ACE application 
+    *               where the logging server resides. Only applicable for debug 
+    *               messages. 
     *
-    * @param message  
+    * @param message The message itself as a local 8 bit string. 
     */
    void messageReceived(Ace::LogServer::Type type, int thread, const QByteArray& message);
 private slots:
@@ -29,15 +37,23 @@ private:
    void readHeader();
    void readMessage();
    /*!
+    * The message type that is currently being processed by this socket. 
     */
    qint8 _type;
    /*!
+    * The thread where the message currently being processed originated on the ACE 
+    * application this client is connected to. 
     */
    qint32 _thread;
    /*!
+    * The size of the message body currently being processed by this socket if 0 or 
+    * greater. If this is -1 then this socket is processing the header of a new 
+    * message. 
     */
    qint32 _sizeToRead {-1};
    /*!
+    * The internal buffer of this socket that temporarily stores new message headers 
+    * and bodies as they are processed. 
     */
    QByteArray _buffer;
 };
