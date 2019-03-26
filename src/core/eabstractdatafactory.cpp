f3fe16c4394e7d90dc8725a5a78bd8890f21c28e
Josh B <4ctrl.alt.del@gmail.com>
2019-03-26 15:25:45 -0700

    Worked on comments.
    Finished refactoring comments for abstract analytic and data factory
    classes.

--- src/core/eabstractdatafactory.cpp
+++ src/core/eabstractdatafactory.cpp
@@ -3,13 +3,8 @@
 
 
 
-using namespace std;
-//
-
-
-
 /*!
- * Pointer to the global instance of this program's data factory. 
+ * Pointer to the global instance of this program's data factory.
  */
 EAbstractDataFactory* EAbstractDataFactory::_instance {nullptr};
 
@@ -19,15 +14,15 @@ EAbstractDataFactory* EAbstractDataFactory::_instance {nullptr};
 
 
 /*!
- * Returns a reference to the global instance of the class that implements this 
- * factory. If no instance has ever been set than an exception is thrown. 
+ * Returns a reference to the global instance of the class that implements this
+ * factory. If no instance has ever been set than an exception is thrown.
  *
- * @return Reference to the global instance of this object's implementation. 
+ * @return Reference to the global instance of this object's implementation.
  */
 EAbstractDataFactory& EAbstractDataFactory::instance()
 {
-   // If the global instance pointer is null then throw an exception, else go the 
-   // next step. 
+   // If the global instance pointer is null then throw an exception, else go the
+   // next step.
    if ( !_instance )
    {
       E_MAKE_EXCEPTION(e);
@@ -36,7 +31,7 @@ EAbstractDataFactory& EAbstractDataFactory::instance()
       throw e;
    }
 
-   // Return reference to the global instance. 
+   // Return reference to the global instance.
    return *_instance;
 }
 
@@ -46,15 +41,17 @@ EAbstractDataFactory& EAbstractDataFactory::instance()
 
 
 /*!
- * Sets the global instance for this object's implementation. If a global instance 
- * already exists then it is deleted and replaced with the new one given. 
+ * Sets the global instance for this object's implementation. If a global
+ * instance already exists then it is deleted and replaced with the new one
+ * given.
  *
- * @param instance Pointer to the new global instance for this factory. 
+ * @param instance Pointer to the new global instance for this factory.
  */
 void EAbstractDataFactory::setInstance(std::unique_ptr<EAbstractDataFactory>&& instance)
 {
-   // Delete any existing global instance and set the global pointer to the new given 
-   // instance. 
+   // Delete any existing global instance and set the global pointer to the new given
+   // instance.
    delete _instance;
    _instance = instance.release();
 }
+
